ae45c2ce6b1423a01c70c88f8ee24d126b3ebee2
Adaobi Frank <adaobi.frank@moduscreate.com>
2018-06-09 14:20:52 +0100

    INITIAL COMMIT

---
+++ app/src/main/java/droidada/com/alcsample30/AdaActivity.kt
@@ -0,0 +1,12 @@
+package droidada.com.alcsample30
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AdaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
